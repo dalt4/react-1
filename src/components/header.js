5ea5e1dc96664e60aafef9fc08cf8cd187483fec
dalt4 <45166154+dalt4@users.noreply.github.com>
2020-03-12 13:06:37 +0300

    lets begin

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const header = () => {
+    return (
+        <div className="wrapper">
+            <h1>First on REACT!</h1>
+        </div>
+)};
+
+export default header;
